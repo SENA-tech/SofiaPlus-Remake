2777c78cef4693230667b7f23999b16f58c3942c
PintoGamer64 <pintogamer64@gmail.com>
2022-07-21 12:47:22 -0500

    About Section Content Added

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,11 @@ import { Route, Routes } from 'react-router-dom';
 
 //Components
 import Home from './Routes/Home/Home'
-import Text from './Routes/Text'
 import Navbar from './Components/Navbar/Navbar';
 import Login from './Components/Login/LoginForm';
 import Footer from './Components/Footer/Footer';
 import About from './Routes/About/About';
 import Suport from './Routes/Suport/Suport';
-import Profile from './User/User';
 
 export default function App() {
 
