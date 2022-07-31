7fdf76c4d7d0cb0c73cfac56e31e9bfae92061df
PintoGamer64 <pintogamer64@gmail.com>
2022-07-31 17:03:09 -0500

    Version 1.5 Pre Fixed

--- src/Hooks/useFecth.jsx
+++ src/Hooks/useFecth.jsx
@@ -35,7 +35,7 @@ export default function useFecth() {
 
         console.log('Login');
 
-        fetch('http://localhost:4000/', {
+        fetch('https://SofiaPlus-API.11-cardozo-joan.repl.co', {
             method: "POST",
             headers: {
                 'Content-Type': 'application/json',
@@ -60,7 +60,7 @@ export default function useFecth() {
 
     const register = () => {
 
-        fetch('http://localhost:4000/', {
+        fetch('https://SofiaPlus-API.11-cardozo-joan.repl.co', {
             method: "PUT",
             headers: {
                 'Content-Type': 'application/json',
