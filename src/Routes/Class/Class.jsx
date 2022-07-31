90d598166eff1c5a39333e025f05f0ead1d03493
PintoGamer64 <pintogamer64@gmail.com>
2022-07-31 18:40:28 -0500

    Route Courses Fixed

--- src/Routes/Class/Class.jsx
+++ src/Routes/Class/Class.jsx
@@ -6,96 +6,94 @@ export default function Classmates() {
 
     return (
 
-        <div className="ClassmatesSF">
-            <div className="contenedor-cursos">
-                <div className="contenedor-card-cursos">
-                    <div className="header-cursos">
-                        <div className="red">
-                            <img src="img/27365.png" />
-                        </div>
-                        <h2>Lorem, ipsum.</h2>
-                        <h4>Lorem, ipsum.</h4>
+        <div className="contenedor-cursos">
+            <div className="contenedor-card-cursos">
+                <div className="header-cursos">
+                    <div className="red">
+                        <img src="img/27365.png" />
                     </div>
-                    <div className="descripcion-cursos">
-                        <div className="txt">
-                            <h4>Lorem, ipsum.</h4>
-                            <p>
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident, ad.
-                            </p>
-                            <h4>Lorem, ipsum.</h4>
-                            <p>Lorem, ipsum.</p>
-                            <h4>Lorem, ipsum.</h4>
-                            <p>Lorem, ipsum.</p>
-                            <h4>Lorem, ipsum.</h4>
-                            <p>Lorem, ipsum.</p>
-                        </div>
+                    <h2>Lorem, ipsum.</h2>
+                    <h4>Lorem, ipsum.</h4>
+                </div>
+                <div className="descripcion-cursos">
+                    <div className="txt">
+                        <h4>Lorem, ipsum.</h4>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident, ad.
+                        </p>
+                        <h4>Lorem, ipsum.</h4>
+                        <p>Lorem, ipsum.</p>
+                        <h4>Lorem, ipsum.</h4>
+                        <p>Lorem, ipsum.</p>
+                        <h4>Lorem, ipsum.</h4>
+                        <p>Lorem, ipsum.</p>
+                    </div>
+                    <br />
+                    <div className="mas-informacion">
+                        <button>
+                            <a href="#">Mas Informacion</a>
+                        </button>
 
-                        <div className="mas-informacion">
-                            <button>
-                                <a href="#">Mas Informacion</a>
-                            </button>
-
-                        </div>
                     </div>
                 </div>
-                <div className="contenedor-card-cursos">
-                    <div className="header-cursos">
-                        <div className="red">
-                            <img src="img/27365.png" />
-                        </div>
-                        <h2>Lorem, ipsum.</h2>
-                        <h4>Lorem, ipsum.</h4>
+            </div>
+            <div className="contenedor-card-cursos">
+                <div className="header-cursos">
+                    <div className="red">
+                        <img src="img/27365.png" />
                     </div>
-                    <div className="descripcion-cursos">
-                        <div className="txt">
-                            <h4>Lorem, ipsum.</h4>
-                            <p>
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident, ad.
-                            </p>
-                            <h4>Lorem, ipsum.</h4>
-                            <p>Lorem, ipsum.</p>
-                            <h4>Lorem, ipsum.</h4>
-                            <p>Lorem, ipsum.</p>
-                            <h4>Lorem, ipsum.</h4>
-                            <p>Lorem, ipsum.</p>
-                        </div>
+                    <h2>Lorem, ipsum.</h2>
+                    <h4>Lorem, ipsum.</h4>
+                </div>
+                <div className="descripcion-cursos">
+                    <div className="txt">
+                        <h4>Lorem, ipsum.</h4>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident, ad.
+                        </p>
+                        <h4>Lorem, ipsum.</h4>
+                        <p>Lorem, ipsum.</p>
+                        <h4>Lorem, ipsum.</h4>
+                        <p>Lorem, ipsum.</p>
+                        <h4>Lorem, ipsum.</h4>
+                        <p>Lorem, ipsum.</p>
+                    </div>
+                    <br />
+                    <div className="mas-informacion">
+                        <button>
+                            <a href="#">Mas Informacion</a>
+                        </button>
 
-                        <div className="mas-informacion">
-                            <button>
-                                <a href="#">Mas Informacion</a>
-                            </button>
-
-                        </div>
                     </div>
                 </div>
-                <div className="contenedor-card-cursos">
-                    <div className="header-cursos">
-                        <div className="red">
-                            <img src="img/27365.png" />
-                        </div>
-                        <h2>Lorem, ipsum.</h2>
-                        <h4>Lorem, ipsum.</h4>
+            </div>
+            <div className="contenedor-card-cursos">
+                <div className="header-cursos">
+                    <div className="red">
+                        <img src="img/27365.png" />
                     </div>
-                    <div className="descripcion-cursos">
-                        <div className="txt">
-                            <h4>Lorem, ipsum.</h4>
-                            <p>
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident, ad.
-                            </p>
-                            <h4>Lorem, ipsum.</h4>
-                            <p>Lorem, ipsum.</p>
-                            <h4>Lorem, ipsum.</h4>
-                            <p>Lorem, ipsum.</p>
-                            <h4>Lorem, ipsum.</h4>
-                            <p>Lorem, ipsum.</p>
-                        </div>
+                    <h2>Lorem, ipsum.</h2>
+                    <h4>Lorem, ipsum.</h4>
+                </div>
+                <div className="descripcion-cursos">
+                    <div className="txt">
+                        <h4>Lorem, ipsum.</h4>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident, ad.
+                        </p>
+                        <h4>Lorem, ipsum.</h4>
+                        <p>Lorem, ipsum.</p>
+                        <h4>Lorem, ipsum.</h4>
+                        <p>Lorem, ipsum.</p>
+                        <h4>Lorem, ipsum.</h4>
+                        <p>Lorem, ipsum.</p>
+                    </div>
+                    <br />
+                    <div className="mas-informacion">
+                        <button>
+                            <a href="#">Mas Informacion</a>
+                        </button>
 
-                        <div className="mas-informacion">
-                            <button>
-                                <a href="#">Mas Informacion</a>
-                            </button>
-
-                        </div>
                     </div>
                 </div>
             </div>
