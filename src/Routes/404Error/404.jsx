86995cc8218bd745aec89c537a7dbe468593d8a5
PintoGamer64 <pintogamer64@gmail.com>
2022-08-02 19:50:42 -0500

    404 Error

--- src/Routes/404Error/404.jsx
+++ src/Routes/404Error/404.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
 
+import './404.css'
+
 export default function Error404() {
 
     return (
 
         <div className="ClassmatesSF">
-            <br /><br /><br /><br /><br />
-            <br /><br /><br /><br /><br />
-            <br /><br /><br /><br /><br />
-            <h1>404 - Content not found</h1>
-            <br /><br /><br /><br /><br />
-            <br /><br /><br /><br /><br />
-            <br /><br /><br /><br /><br />
+            <div class="error-404">
+
+                <h1 class="er4">Error - 404</h1>
+                <p class="txt">Lo sentimos, el equipo de desarrollo del SENA esta trabajando para corregir el error, gracias por su
+                    comprension :3
+                </p>
+                <p class="txt">Intentalo mas tarde</p>
+            </div>
         </div>
 
     )
