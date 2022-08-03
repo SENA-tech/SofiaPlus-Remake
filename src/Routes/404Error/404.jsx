fa5ff6dd9b8ec71dcd28bcc7a4e04dc17b1ea90e
PintoGamer64 <pintogamer64@gmail.com>
2022-08-02 19:56:58 -0500

    404 Page Fixed

--- src/Routes/404Error/404.jsx
+++ src/Routes/404Error/404.jsx
@@ -10,10 +10,10 @@ export default function Error404() {
             <div class="error-404">
 
                 <h1 class="er4">Error - 404</h1>
-                <p class="txt">Lo sentimos, el equipo de desarrollo del SENA esta trabajando para corregir el error, gracias por su
+                <p class="txt2">Lo sentimos, el equipo de desarrollo del SENA esta trabajando para corregir el error, gracias por su
                     comprension :3
                 </p>
-                <p class="txt">Intentalo mas tarde</p>
+                <p class="txt2">Intentalo mas tarde</p>
             </div>
         </div>
 
